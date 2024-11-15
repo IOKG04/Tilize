794a97de9f9e22f23e8dc3b02d2cd76b2242f68f
IOKG04 <iokg04@gmail.com>
2024-11-15 22:56:47 +0100

    i mean it works
    to some degree
    now its just the actual function (and configurability) missing, cant be too hard tho

--- src/main.c
+++ src/main.c
@@ -32,12 +32,16 @@
 \************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <SDL2/SDL.h>
 #include "atlas.h"
 #include "gui.h"
 #include "load_png.h"
 #include "rgb24.h"
 #include "texture.h"
+
+#define TILE_SIZE 8
 
 int main(int argc, char **argv){
     if(argc < 2){
@@ -54,38 +58,73 @@ int main(int argc, char **argv){
         goto _clean_and_exit;
     }
 
+    // load and split input image
+    rgb24_texture_t input_image = {};
+    if(load_png(&input_image, argv[1])) goto _clean_and_exit;
+    rgb24_atlas_t input_atlas = {};
+    if(rgb24_atlas_from_texture(&input_atlas, &input_image, TILE_SIZE, TILE_SIZE)) goto _clean_and_exit;
+
     // initialize gui
-    if(gui_setup(256, 256, 2)){
+    if(gui_setup(input_image.width, input_image.height, 1)){
         fprintf(stderr, "Failed to initialize gui in %s, %s, %i\n", __FILE__, __func__, __LINE__);
         goto _clean_and_exit;
     }
 
-    // load input image
-    rgb24_texture_t input_image = {};
-    if(load_png(&input_image, argv[1])){
-        goto _clean_and_exit;
+    // load tiles
+    rgb24_texture_t tiles_texture = {};
+    if(load_png(&tiles_texture, "resources/extiles_8x8.png")) goto _clean_and_exit;
+    rgb24_atlas_t tiles_atlas = {};
+    if(rgb24_atlas_from_texture(&tiles_atlas, &tiles_texture, TILE_SIZE, TILE_SIZE)) goto _clean_and_exit;
+    rgb24_texture_t *tiles = calloc(tiles_atlas.tile_amount_x * tiles_atlas.tile_amount_y, sizeof(*tiles));
+    if(!tiles) goto _clean_and_exit;
+    for(int i = 0; i < tiles_atlas.tile_amount_x * tiles_atlas.tile_amount_y; ++i){
+        if(rgb24_atlas_get_tile(&tiles[i], &tiles_atlas, i % tiles_atlas.tile_amount_x, i / tiles_atlas.tile_amount_x)) goto _clean_and_exit;
     }
 
-    // split up input image
-    rgb24_atlas_t atlas = {};
-    if(rgb24_atlas_from_texture(&atlas, &input_image, 256, 256)){
-        goto _clean_and_exit;
-    }
-
-    // perform demo scene
-    int c = 0;
-    do{
-        rgb24_texture_t ctile = {};
-        rgb24_atlas_get_tile(&ctile, &atlas, c % atlas.tile_amount_x, c / atlas.tile_amount_x);
-        gui_render_texture(0, 0, &ctile);
-        rgb24_texture_destroy(&ctile);
+    // do the thing
+    Uint64 thing_start = SDL_GetTicks64();
+    gui_render_texture(0, 0, &input_image);
+    gui_present();
+    for(int i = 0; i < input_atlas.tile_amount_x * input_atlas.tile_amount_y; ++i){
+        const int inp_x = i % input_atlas.tile_amount_x,
+                  inp_y = i / input_atlas.tile_amount_x;
+        rgb24_texture_t current_tile = {};
+        if(rgb24_atlas_get_tile(&current_tile, &input_atlas, inp_x, inp_y)) goto _clean_and_exit;
+        int min_diff_index = 0,
+            min_diff       = INT_MAX;
+        for(int j = 0; j < tiles_atlas.tile_amount_x * tiles_atlas.tile_amount_y; ++j){
+            int diff = 0;
+            for(int k = 0; k < TILE_SIZE * TILE_SIZE; ++k){
+                diff += abs(current_tile.data[k].r - (int)tiles[j].data[k].r);
+                diff += abs(current_tile.data[k].g - (int)tiles[j].data[k].g);
+                diff += abs(current_tile.data[k].b - (int)tiles[j].data[k].b);
+            }
+            if(diff < min_diff){
+                min_diff = diff;
+                min_diff_index = j;
+            }
+        }
+        rgb24_texture_destroy(&current_tile);
+        gui_render_texture(inp_x * TILE_SIZE, inp_y * TILE_SIZE, &tiles[min_diff_index]);
         gui_present();
-        c = (c + 1) % (atlas.tile_amount_x * atlas.tile_amount_y);
-    }while(getchar() != 'q');
+    }
+    Uint64 thing_end = SDL_GetTicks64();
+    printf("That took %llu ms to complete\n", (long long unsigned)(thing_end - thing_start));
+
+    // wait to exit
+    while(getchar() != '\n');
 
     // clean and exit
     _clean_and_exit:;
-    rgb24_atlas_destroy(&atlas);
+    if(tiles){
+        for(int i = 0; i < tiles_atlas.tile_amount_x * tiles_atlas.tile_amount_y; ++i){
+            rgb24_texture_destroy(&tiles[i]);
+        }
+        free(tiles);
+    }
+    rgb24_atlas_destroy(&tiles_atlas);
+    rgb24_texture_destroy(&tiles_texture);
+    rgb24_atlas_destroy(&input_atlas);
     rgb24_texture_destroy(&input_image);
     gui_free();
     SDL_Quit();
