7cd409a8582b97bc5169dd11f31724797d03c693
IOKG04 <iokg04@gmail.com>
2024-11-21 14:11:58 +0100

    so apparently u *can* download from within premake
    its just that chatgpt didnt show me in any of the 5 times i tried
    well... thats why u read the documentation of the tool and dont just ask chatgpt, kids :3

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,32 @@
+-- download dependencies
+function file_exist(file) -- source: https://stackoverflow.com/a/4991602/19679136
+    local f = io.open(file, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+function download_file(file, url)
+    if not file_exist(file) then
+        io.write("Downloading " .. file)
+        local str, code = http.download(url, file)
+        if code ~= 200 then
+            io.write(" // Failed with http code " .. code .. " - " .. str .. "\n")
+            error()
+        else
+            io.write(" // Done\n")
+        end
+    end
+end
+download_file("src/cJSON.c", "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/heads/master/cJSON.c")
+download_file("src/cJSON.h", "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/heads/master/cJSON.h")
+download_file("src/tinycthread.c", "https://raw.githubusercontent.com/tinycthread/tinycthread/refs/heads/master/source/tinycthread.c")
+download_file("src/tinycthread.h", "https://raw.githubusercontent.com/tinycthread/tinycthread/refs/heads/master/source/tinycthread.h")
+download_file("src/stb_image.h", "https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h")
+download_file("src/stb_image_write.h", "https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image_write.h")
+
 -- premake configuration
 workspace "Tilize"
     configurations { "Debug", "Release" }
